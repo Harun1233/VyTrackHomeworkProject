d38e7c6615b9803669340cad62ae06241897902f
Harun1233 <av.harunonat@gmail.com>
2022-01-28 15:57:00 +0000

    Log in method has implemented successfully

--- src/test/java/com/vytrack/tests/VyTrackTests.java
+++ src/test/java/com/vytrack/tests/VyTrackTests.java
@@ -1,4 +1,28 @@
 package com.vytrack.tests;
 
-public class VyTrackTests {
+import com.vytrack.pages.LogInPage;
+import com.vytrack.utilites.ExcelUtil;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class VyTrackTests extends TestBase {
+
+
+
+    @Test
+    public void test1(){
+        extentLogger=report.createTest("VyTrack Homework Tests");
+
+        LogInPage logInPage=new LogInPage();
+
+        logInPage.loginAsStoreManager();
+
+
+
+
+
+
+    }
+
+
 }
